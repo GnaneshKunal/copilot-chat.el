2407ee7ec188d0bf462fb0d82ac000fd898d46b9
Gnanesh <gnaneshkunal@outlook.com>
2024-10-31 10:30:18 +0530

    Revert to the old gpt-4o model

--- copilot-chat-common.el
+++ copilot-chat-common.el
@@ -124,7 +124,7 @@ Argument NOCONTEXT tells copilot-chat to not send history and buffers."
 
     (json-encode `(("messages" . ,(vconcat messages))
                    ("top_p" . 1)
-                   ("model" . "o1-preview-2024-09-12")
+                   ("model" . "gpt-4o-2024-05-13")
                    ("stream" . t)
                    ("n" . 1)
                    ("intent" . t)
